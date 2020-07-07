680f929729629245761e680be3022362c79a7655
prism <2928673313@qq.com>
2020-07-07 13:05:24 +0800

    first-commit-tarber

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <main-tab-ber/>
+  </div>
+</template>
+
+<script>
+const MainTabBer = () => import('./components/tabber/MainTabBer')
+export default {
+  name: 'App',
+  components: {
+    MainTabBer
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/base.css";
+
+</style>
---
+++ src/components/tabber/MainTabBer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <tab-ber>
+      <tab-ber-item path="/home" activeColor="blue">
+        <img slot="item-icon" src="~assets/imgs/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="~assets/imgs/tabbar/home_active.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-ber-item>
+      <tab-ber-item path="/category" activeColor="green">
+        <img slot="item-icon" src="~assets/imgs/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/imgs/tabbar/category_active.svg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-ber-item>
+      <tab-ber-item path="/cart" activeColor="pink">
+        <img slot="item-icon" src="~assets/imgs/tabbar/cart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/imgs/tabbar/cart_active.svg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-ber-item>
+      <tab-ber-item path="/profile" activeColor="red">
+        <img slot="item-icon" src="~assets/imgs/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/imgs/tabbar/profile_active.svg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-ber-item>
+    </tab-ber>
+  </div>
+</template>
+
+<script>
+  import TabBer from "./TabBer";
+  import TabBerItem from "./TabBerItem";
+  export default {
+    name: "MainTabBer",
+    components: {
+      TabBer,TabBerItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
